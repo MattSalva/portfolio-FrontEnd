4b0116a117c8af69542fbe38f5ae467d1f8bec79
Mathias Salva <45085448+MattSalva@users.noreply.github.com>
2022-08-24 00:29:59 -0300

    Cambio de Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -10,6 +9,9 @@ import { StudyComponent } from './components/study/study.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { ProyectsComponent } from './components/proyects/proyects.component';
 import { HttpClientModule } from '@angular/common/http';
+import {FormsModule} from "@angular/forms";
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -19,12 +21,14 @@ import { HttpClientModule } from '@angular/common/http';
     ExpComponent,
     StudyComponent,
     SkillsComponent,
-    ProyectsComponent
-    
+    ProyectsComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
     HttpClientModule
   ],
   providers: [],
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { PortfolioService } from 'src/app/services/portfolio.service';
+import {PersonaService} from "../../services/persona.service";
+import {persona} from "../../model/persona.model";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
@@ -8,13 +11,18 @@ import { PortfolioService } from 'src/app/services/portfolio.service';
 })
 export class HeaderComponent implements OnInit {
   miPortfolio:any;
-  constructor(private datosPortfolio:PortfolioService) { }
+  persona: persona = new persona("", "", "");
+  constructor(private datosPortfolio:PortfolioService,public personaService: PersonaService, private router: Router) { }
 
   ngOnInit(): void {
     this.datosPortfolio.obtenerDatos().subscribe(data => {
       console.log(data);
       this.miPortfolio=data;
     });
+    this.personaService.getPersona().subscribe(data => {this.persona = data});
   }
 
+  login(){
+    this.router.navigate(['/login'])
+  }
 }
